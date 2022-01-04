26199223c9d80e9170bc23aa3205f3b92a9ac158
Virinas-code <alexcode228@gmail.com>
2022-01-04 10:14:19 +0100

    Server:  Update

--- app/backend/server.py
+++ app/backend/server.py
@@ -4,10 +4,19 @@
 
 Backend: Main app.
 """
+import os
+import sys
+
 import flask
 
+from home.home import home_page
+
+print(sys.path)
+
 app = flask.Flask(__name__, static_folder="ui",
-                  static_url_path="data/", template_folder="app/frontend/")
+                  static_url_path="/data/", template_folder=os.path.abspath("./app/frontend/"))
+
+app.add_url_rule("/", view_func=home_page)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
